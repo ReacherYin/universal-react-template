7099ec8a23ac52d3a5b892c70ea49207b6dd926b
Reacher <reacheryin@gmail.com>
2020-09-02 21:49:53 +0700

    feat: prepare the shared App component in common folder

---
+++ workspaces/common/src/Components/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function App() {
+  return (
+    <React.StrictMode>
+      <View className='App'>
+        <Text>This is an example of an universal React app</Text>
+        <Text>This page is from: common/src/Components/App</Text>
+      </View>
+    </React.StrictMode>
+  )
+}
+
+export default App
